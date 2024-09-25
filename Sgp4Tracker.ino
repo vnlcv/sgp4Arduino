f388ef3f141a3bf8edd874b2ab5cdf5fec4fce82
vnlcv <vnlcv01@gmail.com>
2024-09-25 16:35:55 +0100

    Change TLE array to read tle.txt

--- Sgp4Tracker.ino
+++ Sgp4Tracker.ino
@@ -3,7 +3,11 @@ It takes one satellite TLE/NORAD data from SD card and runs SGP4.
 Output azimuth and elevation angles of the satellite
 The code stops running when the satellite has an elevation angle below 45 degrees.
 */
-#include <Sgp4.h>
+#include <Sgp4.h
+#include <SPI.h>
+#include <SD.h>
+
+#define CS_PIN 10 // Chip Select pin for SD card
 
 //Create object
 Sgp4 sat;
@@ -47,24 +51,44 @@ void Second_Tick()
 
   // Check if elevation is below 45 degrees
   if (sat.satEl < 45.0) {
-    Serial.println("WARNING: Satellite elevation angle is below 45 degrees!");
-    Serial.println("Stopping the program.");
+    Serial.println("Satellite elevation angle is below 45 degrees. Stopping the program.");
     continueRunning = false; // Set flag to stop the main loop
   }
 }
 
 void setup() {
   Serial.begin(115200);
-  Serial.println();
   
   //Set observer location
   sat.site(-0.5276847, 166.9359231, 34); //set site latitude[°], longitude[°] and altitude[m]
 
+  /*
   //Init satellite with TLE data
   char satname[] = "ISS (ZARYA)";
   char tle_line1[] = "1 25544U 98067A   16065.25775256 -.00164574  00000-0 -25195-2 0  9990";  //Line one from the TLE data
   char tle_line2[] = "2 25544  51.6436 216.3171 0002750 185.0333 238.0864 15.54246933988812";  //Line two from the TLE data
-  
+  */
+
+  //Read tle.txt
+  Serial.print("Initializing SD card...");
+  if (!SD.begin(CS_PIN)) {
+      Serial.println("SD card initialization failed!");
+      while (1);
+  }
+  Serial.println("SD card initialization done.");
+
+  File tleFile = SD.open("tle.txt");
+  if (tleFile) {
+      char satname[25] = { 0 };
+      char tle_line1[70] = { 0 };
+      char tle_line2[70] = { 0 };
+
+      tleFile.readBytesUntil('\n', satname, sizeof(satname) - 1);
+      tleFile.readBytesUntil('\n', tle_line1, sizeof(tle_line1) - 1);
+      tleFile.readBytesUntil('\n', tle_line2, sizeof(tle_line2) - 1);
+
+      tleFile.close();
+
   sat.init(satname, tle_line1, tle_line2);     //initialize satellite parameters 
 
   //Display TLE epoch time
@@ -72,6 +96,11 @@ void setup() {
   invjday(jdC, timezone, true, year, mon, day, hr, minute, sec);
   Serial.println("Epoch: " + String(day) + '/' + String(mon) + '/' + String(year) + ' ' + String(hr) + ':' + String(minute) + ':' + String(sec));
   Serial.println();
+  }
+  else {
+      Serial.println("Error opening tle.txt");
+      while (1);
+  }
 }
 
 void loop() {
