97e45b0b2dbf733ca660879b75933edc3b158ba9
vnlcv <vnlcv01@gmail.com>
2024-10-02 13:47:40 +0100

    Add TickTwo files

--- Sgp4Tracker.ino
+++ Sgp4Tracker.ino
@@ -1,127 +1,67 @@
 #include <Sgp4.h>
-#include <SPI.h>
-#include <SD.h>
-
-#define CS_PIN 10 // Chip Select pin for SD card
+#include <Ticker.h>
 
 Sgp4 sat;
+Ticker tkSecond;
+unsigned long unixtime = 1458950400;
+int timezone = 12 ;  //utc + 12
+int framerate;
 
-unsigned long previousMillis = 0;
-const long interval = 1000; // Update every 1000 ms
-
-unsigned long unixtime = 1727085850; //starting unix timestamp
-int timezone = 0;  //utc + 0
-unsigned long framerate;
-
-int year; int mon; int day; int hr; int minute; double sec;
-
-bool continueRunning = true; // Flag to control the main loop
+int  year; int mon; int day; int hr; int minute; double sec;
 
 void Second_Tick()
 {
-  invjday(sat.satJd, timezone, true, year, mon, day, hr, minute, sec);
-  Serial.println("DEBUG: Converted Julian date to calendar date");
-  
+  unixtime += 1;
+       
+  invjday(sat.satJd , timezone,true, year, mon, day, hr, minute, sec);
   Serial.println(String(day) + '/' + String(mon) + '/' + String(year) + ' ' + String(hr) + ':' + String(minute) + ':' + String(sec));
-  Serial.println("azimuth = " + String(sat.satAz) + " elevation = " + String(sat.satEl) + " distance = " + String(sat.satDist));
-  Serial.println("latitude = " + String(sat.satLat) + " longitude = " + String(sat.satLon) + " altitude = " + String(sat.satAlt));
-  
-  Serial.print("DEBUG: Visibility status - ");
+  Serial.println("azimuth = " + String( sat.satAz) + " elevation = " + String(sat.satEl) + " distance = " + String(sat.satDist));
+  Serial.println("latitude = " + String( sat.satLat) + " longitude = " + String( sat.satLon) + " altitude = " + String( sat.satAlt));
+
   switch(sat.satVis){
-    case -2:
-      Serial.println("Under horizon");
-      break;
-    case -1:
-      Serial.println("Daylight");
-      break;
-    default:
-      Serial.println(String(sat.satVis) + " (0:eclipsed - 1000:visible)");
-      break;
-  }
-  
+              case -2:
+                  Serial.println("Visible : Under horizon");
+                  break;
+              case -1:
+                  Serial.println("Visible : Daylight");
+                  break;
+              default:
+                  Serial.println("Visible : " + String(sat.satVis));   //0:eclipsed - 1000:visible
+                  break;
+          }
+
   Serial.println("Framerate: " + String(framerate) + " calc/sec");
   Serial.println();
      
-  framerate = 0;
-
-  if (sat.satEl < 45.0) {
-    Serial.println("WARNING: Satellite elevation angle is below 45 degrees. Stopping the program.");
-    continueRunning = false;
-  }
+  framerate=0;
 }
 
 void setup() {
+
   Serial.begin(115200);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
-  Serial.println("DEBUG: Serial communication initialized");
+  Serial.println();
   
-  sat.site(-0.5276847, 166.9359231, 34);
-  Serial.println("DEBUG: Observer location set");
+  sat.site(-0.5276847,166.9359231,34); //set site latitude[°], longitude[°] and altitude[m]
 
-  Serial.print("Initializing SD card...");
-  if (!SD.begin(CS_PIN)) {
-    Serial.println("initialization failed!");
-    Serial.println("DEBUG: Check SD card connection and CS_PIN definition");
-    while (1);
-  }
-  Serial.println("initialization done.");
+  char satname[] = "ISS (ZARYA)";
+  char tle_line1[] = "1 25544U 98067A   16065.25775256 -.00164574  00000-0 -25195-2 0  9990";  //Line one from the TLE data
+  char tle_line2[] = "2 25544  51.6436 216.3171 0002750 185.0333 238.0864 15.54246933988812";  //Line two from the TLE data
+  
+  sat.init(satname,tle_line1,tle_line2);     //initialize satellite parameters 
 
-  File tleFile = SD.open("tle.txt");
-  if (tleFile) {
-    Serial.println("DEBUG: tle.txt opened successfully");
-    char satname[25] = { 0 };
-    char tle_line1[70] = { 0 };
-    char tle_line2[70] = { 0 };
+  //Display TLE epoch time
+  double jdC = sat.satrec.jdsatepoch; //retrieves Julian date of the satellite's epoch from the 'sat' object
+  invjday(jdC , timezone, true, year, mon, day, hr, minute, sec); //converts the Julian date (jdC) to calendar date
+  Serial.println("Epoch: " + String(day) + '/' + String(mon) + '/' + String(year) + ' ' + String(hr) + ':' + String(minute) + ':' + String(sec));
+  Serial.println();
 
-    tleFile.readBytesUntil('\n', satname, sizeof(satname) - 1);
-    tleFile.readBytesUntil('\n', tle_line1, sizeof(tle_line1) - 1);
-    tleFile.readBytesUntil('\n', tle_line2, sizeof(tle_line2) - 1);
-
-    tleFile.close();
-    Serial.println("DEBUG: TLE data read and file closed");
-
-    Serial.println("DEBUG: Satellite name: " + String(satname));
-    Serial.println("DEBUG: TLE Line 1: " + String(tle_line1));
-    Serial.println("DEBUG: TLE Line 2: " + String(tle_line2));
-
-    if (sat.init(satname, tle_line1, tle_line2)) {
-      Serial.println("DEBUG: Satellite parameters initialized successfully");
-    } else {
-      Serial.println("ERROR: Failed to initialize satellite parameters");
-      while(1);
-    }
-
-    double jdC = sat.satrec.jdsatepoch;
-    invjday(jdC, timezone, true, year, mon, day, hr, minute, sec);
-    Serial.println("Epoch: " + String(day) + '/' + String(mon) + '/' + String(year) + ' ' + String(hr) + ':' + String(minute) + ':' + String(sec));
-    Serial.println();
-  }
-  else {
-    Serial.println("Error opening tle.txt");
-    Serial.println("DEBUG: Check if tle.txt exists on the SD card");
-    while (1);
-  }
+  tkSecond.attach(1,Second_Tick);
 }
 
+
 void loop() {
-  if (!continueRunning) {
-    Serial.println("DEBUG: Program stopped due to low elevation angle");
-    while(1);
-  }
 
-  unsigned long currentMillis = millis();
+  sat.findsat(unixtime);
+  framerate += 1;
 
-  if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
-    
-    Serial.println("DEBUG: Calculating satellite position for unixtime: " + String(unixtime));
-    sat.findsat(unixtime);
-    Second_Tick();
-    
-    unixtime += 1;
-  }
-
-  framerate++;
 }
---
+++ SgpTrackerTickTwo/SgpTrackerTickTwo.ino
@@ -0,0 +1,75 @@
+#include <Sgp4.h>
+#include <TickTwo.h>
+
+Sgp4 sat;
+
+unsigned long unixtime = 1727803310;
+int timezone = 0;  //utc 
+int framerate;
+
+int year; int mon; int day; int hr; int minute; double sec;
+
+void Second_Tick();
+TickTwo timer(Second_Tick, 1000); // Call Second_Tick every 1000ms (1 second)
+
+void Second_Tick()
+{
+  unixtime += 1;
+       
+  invjday(sat.satJd, timezone, true, year, mon, day, hr, minute, sec);
+  Serial.println(String(day) + '/' + String(mon) + '/' + String(year) + ' ' + String(hr) + ':' + String(minute) + ':' + String(sec));
+  Serial.println("azimuth = " + String(sat.satAz) + " elevation = " + String(sat.satEl) + " distance = " + String(sat.satDist));
+  Serial.println("latitude = " + String(sat.satLat) + " longitude = " + String(sat.satLon) + " altitude = " + String(sat.satAlt));
+
+  switch(sat.satVis){
+    case -2:
+      Serial.println("Visible : Under horizon");
+      break;
+    case -1:
+      Serial.println("Visible : Daylight");
+      break;
+    default:
+      Serial.println("Visible : " + String(sat.satVis));   //0:eclipsed - 1000:visible
+      break;
+  }
+
+  Serial.println("Framerate: " + String(framerate) + " calc/sec");
+  Serial.println();
+     
+  framerate = 0;
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  
+  sat.site(51.581468, -1.31088, 120); //set site latitude[°], longitude[°] and altitude[m]
+
+  char satname[] = "ISS (ZARYA)";
+  char tle_line1[] = "1 25544U 98067A   24276.37395833  .00047432  00000+0  83119-3 0  9996";  //Line one from the TLE data
+  char tle_line2[] = "2 25544  51.6382 144.4720 0007356  54.4438 303.9262 15.50074020475160";  //Line two from the TLE data
+
+  /*sat.site(0, -158, 120); //set site latitude[°], longitude[°] and altitude[m]
+
+  char satname[] = "Reference";
+  char tle_line1[] = "1 25544U 98067A   24276.37395833  .00047432  00000+0  83119-3 0  9996";  //Line one from the TLE data
+  char tle_line2[] = "2 25544  00.0000 144.4720 0000000  54.4438 303.9262 15.50074020475160";  //Line two from the TLE data
+  */
+  
+  sat.init(satname, tle_line1, tle_line2);     //initialize satellite parameters 
+
+  //Display TLE epoch time
+  double jdC = sat.satrec.jdsatepoch;
+  invjday(jdC, timezone, true, year, mon, day, hr, minute, sec);
+  Serial.println("Epoch: " + String(day) + '/' + String(mon) + '/' + String(year) + ' ' + String(hr) + ':' + String(minute) + ':' + String(sec));
+  Serial.println();
+
+  timer.start(); // Start the TickTwo timer
+}
+
+void loop() {
+  timer.update(); // Update the TickTwo timer
+
+  sat.findsat(unixtime);
+  framerate += 1;
+}
